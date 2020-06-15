7eb4f087eb287a50996be511a58e9229cda6598d
Hans Martin <hansmrtn@gmail.com>
2020-06-15 11:56:15 -0700

    updated executable for extended air sensor db

--- createAirSensor_extended_exec.R
+++ createAirSensor_extended_exec.R
@@ -30,26 +30,23 @@ if ( interactive() ) {
   
 } else {
   
-  # Set up OptionParser
-  library(optparse)
-  
   option_list <- list(
-    make_option(
+    optparse::make_option(
       c("-o","--archiveBaseDir"), 
       default = getwd(), 
       help = "Output base directory for generated .RData files [default = \"%default\"]"
     ),
-    make_option(
+    optparse::make_option(
       c("-l","--logDir"), 
       default = getwd(), 
       help = "Output directory for generated .log file [default = \"%default\"]"
     ),
-    make_option(
+    optparse::make_option(
       c("-n","--collectionName"), 
       default = "scaqmd", 
       help = "Name associated with this collection of sensors [default = \"%default\"]"
     ),
-    make_option(
+    optparse::make_option(
       c("-V","--version"), 
       action="store_true", 
       default = FALSE, 
@@ -58,7 +55,7 @@ if ( interactive() ) {
   )
   
   # Parse arguments
-  opt <- parse_args(OptionParser(option_list=option_list))
+  opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
   
 }
 
@@ -82,14 +79,14 @@ if ( !dir.exists(opt$logDir) )
 # ----- Set up logging ---------------------------------------------------------
 
 logger.setup(
-  traceLog = file.path(opt$logDir, paste0("createAirSensor_extended_",opt$collectionName,"_TRACE.log")),
-  debugLog = file.path(opt$logDir, paste0("createAirSensor_extended_",opt$collectionName,"_DEBUG.log")), 
-  infoLog  = file.path(opt$logDir, paste0("createAirSensor_extended_",opt$collectionName,"_INFO.log")), 
-  errorLog = file.path(opt$logDir, paste0("createAirSensor_extended_",opt$collectionName,"_ERROR.log"))
+  traceLog = file.path(opt$logDir, paste0("createAirSensor_",opt$collectionName,"_extended_TRACE.log")),
+  debugLog = file.path(opt$logDir, paste0("createAirSensor_",opt$collectionName,"_extended_DEBUG.log")), 
+  infoLog  = file.path(opt$logDir, paste0("createAirSensor_",opt$collectionName,"_extended_INFO.log")), 
+  errorLog = file.path(opt$logDir, paste0("createAirSensor_",opt$collectionName,"_extended_ERROR.log"))
 )
 
 # For use at the very end
-errorLog <- file.path(opt$logDir, paste0("createAirSensor_extended_",opt$collectionName,"_ERROR.log"))
+errorLog <- file.path(opt$logDir, paste0("createAirSensor_",opt$collectionName,"_extended_ERROR.log"))
 
 # Silence other warning messages
 options(warn=-1) # -1=ignore, 0=save/print, 1=print, 2=error
@@ -105,122 +102,138 @@ logger.debug('Command line options:\n\n%s\n', optionsString)
 
 # ------ Get labels ------------------------------------------------------------
 
-# All datestamps are UTC
-timezone <- "UTC"
-
-result <- try({
-  
-  # Get dates and date stamps
-  now <- lubridate::now(tzone = timezone)
-  now_m45 <- now - lubridate::ddays(45)
-  cur_monthStart <- lubridate::floor_date(now, "month") - lubridate::ddays(1)
-  cur_monthEnd <- lubridate::ceiling_date(now, "month") + lubridate::ddays(1)
-  cur_monthStamp <- strftime(now, "%Y%m", tz = timezone)
-  prev_midMonth <- cur_monthStart - lubridate::ddays(14)
-  prev_monthStart <- lubridate::floor_date(prev_midMonth, "month") - lubridate::ddays(1)
-  prev_monthEnd <- lubridate::ceiling_date(prev_midMonth, "month") + lubridate::ddays(1)
-  prev_monthStamp <- strftime(prev_midMonth, "%Y%m", tz = timezone)
-  cur_yearStamp <- strftime(now, "%Y", tz = timezone)
-  prev_yearStamp <- strftime(prev_midMonth, "%Y", tz = timezone)
-  
-  logger.trace("Setting up data directories")
-  latestDataDir <- paste0(opt$archiveBaseDir, "/airsensor/latest")
-  cur_monthlyDir <- paste0(opt$archiveBaseDir, "/airsensor/", cur_yearStamp)
-  prev_monthlyDir <- paste0(opt$archiveBaseDir, "/airsensor/", prev_yearStamp)
-  
-  logger.trace("latestDataDir = %s", latestDataDir)
-  logger.trace("cur_monthlyDir = %s", cur_monthlyDir)
-  logger.trace("prev_monthlyDir = %s", prev_monthlyDir)
-  
-  if ( !dir.exists(cur_monthlyDir) )
-    dir.create(cur_monthlyDir, showWarnings = FALSE, recursive = TRUE)
-  
-  if ( !dir.exists(prev_monthlyDir) )
-    dir.create(prev_monthlyDir, showWarnings = FALSE, recursive = TRUE)
-  
-}, silent=TRUE)
-
-# Handle errors
-if ( "try-error" %in% class(result) ) {
-  msg <- paste("Error in PAS file: ", geterrmessage())
-  logger.fatal(msg)
-  stop(msg)
-}
+tryCatch(
+  expr = {
+    # All datestamps are UTC
+    timezone <- "UTC"
+    
+    # Get dates and date stamps
+    now <- lubridate::now(tzone = timezone)
+    now_m45 <- now - lubridate::ddays(45)
+    cur_monthStart <- lubridate::floor_date(now, "month") - lubridate::ddays(1)
+    cur_monthEnd <- lubridate::ceiling_date(now, "month") + lubridate::ddays(1)
+    cur_monthStamp <- strftime(now, "%Y%m", tz = timezone)
+    prev_midMonth <- cur_monthStart - lubridate::ddays(14)
+    prev_monthStart <- lubridate::floor_date(prev_midMonth, "month") - lubridate::ddays(1)
+    prev_monthEnd <- lubridate::ceiling_date(prev_midMonth, "month") + lubridate::ddays(1)
+    prev_monthStamp <- strftime(prev_midMonth, "%Y%m", tz = timezone)
+    cur_yearStamp <- strftime(now, "%Y", tz = timezone)
+    prev_yearStamp <- strftime(prev_midMonth, "%Y", tz = timezone)
+    
+    logger.trace("Setting up data directories")
+    latestDataDir <- paste0(opt$archiveBaseDir, "/airsensor/latest")
+    cur_monthlyDir <- paste0(opt$archiveBaseDir, "/airsensor/", cur_yearStamp)
+    prev_monthlyDir <- paste0(opt$archiveBaseDir, "/airsensor/", prev_yearStamp)
+    
+    logger.trace("latestDataDir = %s", latestDataDir)
+    logger.trace("cur_monthlyDir = %s", cur_monthlyDir)
+    logger.trace("prev_monthlyDir = %s", prev_monthlyDir)
+    
+    if ( !dir.exists(cur_monthlyDir) )
+      dir.create(cur_monthlyDir, showWarnings = FALSE, recursive = TRUE)
+    
+    if ( !dir.exists(prev_monthlyDir) )
+      dir.create(prev_monthlyDir, showWarnings = FALSE, recursive = TRUE)
+  },
+  error = function(e) {
+    msg <- paste("Error in PAS file: ", e)
+    logger.fatal(msg)
+    stop(msg)
+  }
+)
 
 # ------ Create 45-day airsensor objects ---------------------------------------
 
-result <- try({
-  
-  latest7Path <- file.path(latestDataDir, paste0("airsensor_", opt$collectionName, "_latest7.rda"))
-  latest45Path <- file.path(latestDataDir, paste0("airsensor_", opt$collectionName, "_latest45.rda"))
-  cur_monthPath <- file.path(cur_monthlyDir, paste0("airsensor_", opt$collectionName, "_", cur_monthStamp, ".rda"))
-  prev_monthPath <- file.path(prev_monthlyDir, paste0("airsensor_", opt$collectionName, "_", prev_monthStamp, ".rda"))
-  
-  logger.trace("Loading %s", latest7Path)
-
-  # Load latest7
-  if ( file.exists(latest7Path) ) {
-    latest7 <- get(load(latest7Path))
-  } else {
-    logger.trace("Skipping %s, missing %s", opt$collectionName, latest7Path)
-    next
-  }
-  
-  # Combine latest7 and latest45
-  if ( file.exists(latest45Path) ) {
-    logger.trace("Loading %s", latest45Path)
-    latest45 <- get(load(latest45Path))
-    logger.trace("Joining latest7 and latest45")
-    monitorIDs <- union(latest45$meta$monitorID, latest7$meta$monitorID)
-    logger.trace("monitorIDs = %s", paste0(monitorIDs, collapse = ", "))
-    # Handle erros by just defaulting to latest7
-    result <- try({
-      airsensor_full <- PWFSLSmoke::monitor_join(latest45, latest7, monitorIDs) 
-    }, silent = TRUE)
-    if ( "try-error" %in% class(result) ) {
-      airsensor_full <- latest7
-    }
-  } else {
-    airsensor_full <- latest7 # default when starting from scratch
-  }
-  
-  logger.trace("Update and save %s", latest45Path)
-
-  # Update the latest45 file
-  airsensor <- 
-    airsensor_full %>%
-    PWFSLSmoke::monitor_subset(tlim = c(now_m45, now))
-  
-  save(list="airsensor", file = latest45Path)
-  
-  logger.trace("Update and save %s", cur_monthPath)
-
-  # Update the current month file
-  airsensor <- 
-    airsensor_full %>%
-    PWFSLSmoke::monitor_subset(tlim = c(cur_monthStart, cur_monthEnd))
-  
-  save(list="airsensor", file = cur_monthPath)
-  
-  # Update the previous month file until 7-days into the current month
-  if ( lubridate::day(now) < 7 ) {
-    logger.trace("Update and save %s", prev_monthPath)
-    airsensor <- 
-      airsensor_full %>%
-      PWFSLSmoke::monitor_subset(tlim = c(prev_monthStart, prev_monthEnd))
+tryCatch(
+  expr = {
     
-    save(list="airsensor", file = prev_monthPath)
+    latest7Path <- file.path(latestDataDir, paste0("airsensor_", opt$collectionName, "_latest7.rda"))
+    latest45Path <- file.path(latestDataDir, paste0("airsensor_", opt$collectionName, "_latest45.rda"))
+    cur_monthPath <- file.path(cur_monthlyDir, paste0("airsensor_", opt$collectionName, "_", cur_monthStamp, ".rda"))
+    prev_monthPath <- file.path(prev_monthlyDir, paste0("airsensor_", opt$collectionName, "_", prev_monthStamp, ".rda"))
+    
+    logger.trace("Loading %s", latest7Path)
+    
+    # Load latest7
+    if ( file.exists(latest7Path) ) {
+      latest7 <- get(load(latest7Path))
+    } else {
+      logger.trace("Skipping %s, missing %s", opt$collectionName, latest7Path)
+      next
+    }
+    
+    # Combine latest7 and latest45
+    if ( file.exists(latest45Path) ) {
+      logger.trace("Loading %s", latest45Path)
+      latest45 <- get(load(latest45Path))
+      logger.trace("Joining latest7 and latest45")
+      monitorIDs <- union(latest45$meta$monitorID, latest7$meta$monitorID)
+      logger.trace("monitorIDs = %s", paste0(monitorIDs, collapse = ", "))
+      # Handle erros by just defaulting to latest7
+      result <- try({
+        airsensor_full <- PWFSLSmoke::monitor_join(latest45, latest7, monitorIDs) 
+      }, silent = TRUE)
+      if ( "try-error" %in% class(result) ) {
+        airsensor_full <- latest7
+      }
+    } else {
+      airsensor_full <- latest7 # default when starting from scratch
+    }
+    
+    # Save latest 45 sensor data
+    tryCatch(
+      expr = {
+        # Update the latest45 file
+        airsensor <- 
+          airsensor_full %>%
+          PWFSLSmoke::monitor_subset(tlim = c(now_m45, now))
+        
+        logger.trace("Update and save %s", latest45Path)
+        
+        save(list="airsensor", file = latest45Path)
+      }, 
+      error = function(e) {
+        # Catch errors one level up 
+        stop(e)
+      }
+    )
+    
+    # Save current month data
+    tryCatch(
+      expr = {
+        # Update the current month file
+        airsensor <- 
+          airsensor_full %>%
+          PWFSLSmoke::monitor_subset(tlim = c(cur_monthStart, cur_monthEnd))
+        
+        logger.trace("Update and save %s", cur_monthPath)
+        
+        save(list="airsensor", file = cur_monthPath)
+      }, 
+      error = function(e) {
+        # Catch errors up one level 
+        stop(e)
+      }
+    )
+    
+  }, 
+  error = function(e) {
+    msg <- paste("Airsensor creation error: ", e)
+    logger.fatal(msg)
   }
-  
-}, silent=TRUE)
+)
 
-# Handle errors
-if ( "try-error" %in% class(result) ) {
-  msg <- paste("Error creating monthly airsensor file: ", geterrmessage())
-  logger.fatal(msg)
-} else {
-  # Guarantee that the errorLog exists
-  if ( !file.exists(errorLog) ) dummy <- file.create(errorLog)
-  logger.info("Completed successfully!")
+if ( lubridate::day(now) < 7 ) {
+  logger.trace("Update and save %s", prev_monthPath)
+  airsensor <- 
+    airsensor_full %>%
+    PWFSLSmoke::monitor_subset(tlim = c(prev_monthStart, prev_monthEnd))
+  
+  save(list="airsensor", file = prev_monthPath)
 }
+
+# Guarantee that the errorLog exists
+if ( !file.exists(errorLog) ) dummy <- file.create(errorLog)
+logger.info("Completed successfully!")
+
 
